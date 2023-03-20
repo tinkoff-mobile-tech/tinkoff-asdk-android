e133dd9909aeeeed0e38bdde665417da2d941c16
jqwout <jqwoutt@gmail.com>
2023-03-20 14:33:00 +0300

    MC-8549 - new card marker & card scanner marker

--- ui/src/main/java/ru/tinkoff/acquiring/sdk/models/options/screen/SavedCardsOptions.kt
+++ ui/src/main/java/ru/tinkoff/acquiring/sdk/models/options/screen/SavedCardsOptions.kt
@@ -3,6 +3,8 @@ package ru.tinkoff.acquiring.sdk.models.options.screen
 import android.os.Parcel
 import android.os.Parcelable
 import ru.tinkoff.acquiring.sdk.TinkoffAcquiring
+import ru.tinkoff.acquiring.sdk.models.options.CustomerOptions
+import ru.tinkoff.acquiring.sdk.models.options.FeaturesOptions
 
 /**
  * Настройки экрана сохраненных карт
@@ -11,11 +13,32 @@ import ru.tinkoff.acquiring.sdk.TinkoffAcquiring
  */
 class SavedCardsOptions : BaseCardsOptions<SavedCardsOptions>, Parcelable {
 
+    internal var anotherCard: Boolean = false
+    internal var addNewCard: Boolean = false
+
     /**
      * [TinkoffAcquiring.savedCardsOptions]
      */
     internal constructor() : super()
-    private constructor(parcel: Parcel) : super(parcel)
+
+    private constructor(parcel: Parcel) : super(parcel) {
+        parcel.run {
+            anotherCard = readByte().isTrue()
+            addNewCard = readByte().isTrue()
+        }
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        super.writeToParcel(parcel, flags)
+        parcel.run {
+            writeByte(anotherCard.toByte())
+            writeByte(addNewCard.toByte())
+        }
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
 
     override fun setOptions(options: SavedCardsOptions.() -> Unit): SavedCardsOptions {
         return SavedCardsOptions().apply(options)
