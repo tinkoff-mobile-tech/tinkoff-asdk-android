5223691f62cc790cb972f8baba824bc5ba1072af
jqwout <jqwoutt@gmail.com>
2023-04-14 17:34:37 +0300

    MC-8852 - tpay domain dev

---
+++ ui/src/test/java/ru/tinkoff/acquiring/sdk/payment/TpayProcessTest.kt
@@ -0,0 +1,75 @@
+package ru.tinkoff.acquiring.sdk.payment
+
+import common.assertByClassName
+import kotlinx.coroutines.*
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import ru.tinkoff.acquiring.sdk.models.options.screen.PaymentOptions
+import ru.tinkoff.acquiring.sdk.payment.methods.TpayMethods
+import ru.tinkoff.acquiring.sdk.payment.pooling.GetStatusPooling
+
+class TpayProcessTest {
+
+    @Test
+    fun `test recreate process`() {
+        TpayProcessEnv().runWithEnv(
+            given = { setInitAndLingResult(defaultPaymentId) },
+            `when` = {
+                tpayProcess.stop()
+                tpayProcess.start(PaymentOptions(), versionTpay, defaultPaymentId)
+            },
+            then = {
+                assertByClassName(
+                    TpayPaymentState.NeedChooseOnUi(paymentId = defaultPaymentId, deeplink),
+                    tpayProcess.state.value
+                )
+            },
+        )
+    }
+
+    @Test
+    fun `test recreate process after failure`() {
+        TpayProcessEnv().runWithEnv(
+            given = {},
+            `when` = {
+                tpayProcess.start(PaymentOptions(), versionTpay, defaultPaymentId)
+                assertByClassName(TpayPaymentState.PaymentFailed(defaultPaymentId,IllegalStateException()), tpayProcess.state.value)
+                setInitAndLingResult(defaultPaymentId)
+                tpayProcess.start(PaymentOptions(), versionTpay, defaultPaymentId)
+            },
+            then = {
+                assertByClassName(TpayPaymentState.NeedChooseOnUi(defaultPaymentId,deeplink), tpayProcess.state.value)
+            },
+        )
+    }
+}
+
+internal class TpayProcessEnv {
+    var getStatusPooling: GetStatusPooling = mock()
+    var getTpayLinkMethods: TpayMethods = mock()
+    var scope: CoroutineScope = CoroutineScope(Dispatchers.Unconfined)
+    val tpayProcess = TpayProcess(getStatusPooling, getTpayLinkMethods, scope)
+
+    var defaultPaymentId : Long = 1L
+    var deeplink : String = "link"
+    var versionTpay : String = "2"
+
+    suspend fun setInitAndLingResult(paymentId: Long = defaultPaymentId) {
+        whenever(getTpayLinkMethods.init(any())).thenReturn(paymentId)
+        whenever(getTpayLinkMethods.tinkoffPayLink(any(), any())).thenReturn(deeplink)
+    }
+}
+
+internal fun TpayProcessEnv.runWithEnv(
+    given: suspend TpayProcessEnv.() -> Unit,
+    `when`: suspend TpayProcessEnv.() -> Unit,
+    then: suspend TpayProcessEnv.() -> Unit
+) {
+    runBlocking {
+        launch { given.invoke(this@runWithEnv) }.join()
+        launch { `when`.invoke(this@runWithEnv) }.join()
+        launch { then.invoke(this@runWithEnv) }.join()
+    }
+}
