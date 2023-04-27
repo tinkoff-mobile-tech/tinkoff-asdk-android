32d0a3b8ba87b31ffda1dc06ae5bf1cb0500ddb3
jqwout <jqwoutt@gmail.com>
2023-04-27 16:05:00 +0300

    EACQAPW-4621 фикс отправки параметров в рекуренте

--- ui/src/main/java/ru/tinkoff/acquiring/sdk/payment/methods/InitMethods.kt
+++ ui/src/main/java/ru/tinkoff/acquiring/sdk/payment/methods/InitMethods.kt
@@ -12,22 +12,32 @@ interface InitMethods {
 
     suspend fun init(paymentOptions: PaymentOptions, email: String?): InitResponse
 
+    suspend fun initRecurrent(paymentOptions: PaymentOptions, email: String?): InitResponse
 }
 
 internal class InitMethodsSdkImpl(private val acquiringSdk: AcquiringSdk) : InitMethods {
 
     override suspend fun init(paymentOptions: PaymentOptions, email: String?): InitResponse {
-        return acquiringSdk.configureInit(paymentOptions, email).execute()
+        return acquiringSdk.configureInit(paymentOptions, false, email).execute()
+    }
+
+    override suspend fun initRecurrent(
+        paymentOptions: PaymentOptions,
+        email: String?
+    ): InitResponse {
+        return acquiringSdk.configureInit(paymentOptions, true, email).execute()
     }
 
     private fun AcquiringSdk.configureInit(
         paymentOptions: PaymentOptions,
+        isRecurrent: Boolean,
         email: String? = null
     ) = init {
         configure(paymentOptions)
         if (paymentOptions.features.duplicateEmailToReceipt && email.isNullOrEmpty().not()) {
             receipt?.email = email
         }
+        this.recurrent = isRecurrent
     }
 
 }
